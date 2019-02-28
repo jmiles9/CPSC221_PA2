ef3772129ca5d128d09c6e591a6cdac27e35b51b
Miles Justice <g5p1b@anvil.ugrad.cs.ubc.ca>
2019-02-28 13:26:45 -0800

    looks like dfs is working but theres something wrong with the tolerance or pixel choice near the very end of the dfs

--- pa2/filler.h
+++ pa2/filler.h
@@ -27,6 +27,7 @@ using namespace std;
  */
 namespace filler
 {
+        bool withinTolerance(HSLAPixel * p, HSLAPixel * c, int tolerance);
         /**
          * Performs a flood fill on the given image using a stripe,
          * employing a depth-first-search approach.
