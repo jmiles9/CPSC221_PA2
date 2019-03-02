f8a43b70171d94958c31a3ef8c3100d271b3b212
Miles Justice <g5p1b@anvil.ugrad.cs.ubc.ca>
2019-03-01 17:08:00 -0800

    sadfas

--- pa2/borderColorPicker.cpp
+++ pa2/borderColorPicker.cpp
@@ -2,12 +2,24 @@
 
 borderColorPicker::borderColorPicker(HSLAPixel fillColor, PNG & img, double tolerance,HSLAPixel center)
 {
-/* Your code here! */
+    im = img;
+    color = fillColor;
+    tol = tolerance;
+    ctr = center;
 }
 
 HSLAPixel borderColorPicker::operator()(int x, int y)
 {
+    HSLAPixel * p = im.getPixel(x, y);
+    bool nearEdge = false;
+    for(int i = -3; i <=3; i ++){
+        for(int j = -3; j <= 3; j ++){
+            HSLAPixel * temp = im.getPixel(x, y);
+            if(temp->dist(*p) <= 3) nearEdge = true;
+        }
+    }
 
-/* Your code here! */
-    
+    if(!nearEdge) return *p;
+
+    return color;
 }
